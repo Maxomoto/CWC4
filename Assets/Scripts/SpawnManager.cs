2ffb109208bed63bc2184cf8f5180834b23680e6
Maxomoto <maximcgregor@gmail.com>
2023-08-21 13:00:15 +0100

    Initial commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    public GameObject powerupPrefab;
+    private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup();
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+        
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefabs[enemyIndex], GenerateSpawnPosition(), enemyPrefabs[enemyIndex].transform.rotation);
+        }
+    }
+
+    void SpawnPowerup()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPositionX = Random.Range(-spawnRange, spawnRange);
+        float spawnPositionZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPositionX, 0, spawnPositionZ);
+
+        return randomPos;
+    }
+}
